735bdc9d72d272abf332b99cafdfadc98ccd180b
yank.nvim <yanknvim@gmail.com>
2024-06-22 20:45:43 +0900

    Revert "Revert "Add reset""
    This reverts commit 30bbdd8cd7a87b7d7ccbdc6bb46064cacbe21631.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
-<script>
+<script lang="ts">
 	import Counter from "$lib/Counter.svelte";
+
+	let lifeA;
+	let lifeB;
+
+	const resetLife = () => {
+		lifeA = 20;
+		lifeB = 20;
+	};
 </script>
 
-<div class="flex max-md:flex-col p-5 gap-5 h-dvh">
-	<Counter rotate=true />
-	<Counter />
+<div class="flex max-md:flex-col p-5 gap-5 h-dvh items-center">
+	<Counter rotate=true  bind:life={lifeA} />
+	<button class="shrink-0 bg-gray-100 h-fit w-fit px-3 py-2 rounded-xl" on:click={resetLife}>Reset</button>
+	<Counter bind:life={lifeB} />
 </div>
 
